5df753433dee5cbba1f6a2c0ccde0025d1f4844a
realashrafi <ccvali@outlook.com>
2024-07-10 12:52:29 +0330

    init

--- login-demo/src/App.tsx
+++ login-demo/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
 
 function App() {
-  return (
-    <>
-
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route index/>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
