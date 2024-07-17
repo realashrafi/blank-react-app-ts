074bcb6d8e6ca31edd8ef224807a85b3c4399d3d
ali ashrafi <ccvali@outlook.com>
2024-07-17 14:12:26 +0330

    -- update

--- login-demo/src/App.tsx
+++ login-demo/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import AuthClassicLayout from "./provider/AuthProvider";
